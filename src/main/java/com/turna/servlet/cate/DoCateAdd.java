8fc1384bf04a6f0b88b8388a9c0ec0372573882c
fu.zhirao <fu.zhirao@CD0026.ad.cdrive-inc.jp>
2022-05-18 16:53:15 +0900

    カテゴリの追加

---
+++ src/main/java/com/turna/servlet/cate/DoCateAdd.java
@@ -0,0 +1,41 @@
+package com.turna.servlet.cate;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.turna.entity.Category;
+import com.turna.service.CategoryDao;
+
+/**
+ * @author fu.zhirao
+ */
+@WebServlet("/manager/admin_docateadd")
+public class DoCateAdd extends HttpServlet {
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// 文字コードを設定する
+		request.setCharacterEncoding("UTF-8");
+		response.setContentType("text/html;charset=UTF-8");
+
+		// パラメターを取得
+		int pid = Integer.parseInt(request.getParameter("parentId"));
+		String name = request.getParameter("className");
+
+		// インスタンス化
+		Category cate = new Category(0, name, pid,null,null);
+		// DBに入れる
+		CategoryDao.insert(cate);
+		//遷移先
+		response.sendRedirect("admin_cateselect");
+	}
+
+}
